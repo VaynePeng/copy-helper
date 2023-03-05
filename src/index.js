649e65fb8365284cf82421834e34c4592a6f8da9
VaynePeng <vaynepeng@gmail.com>
2023-03-06 02:59:10 +0800

    Feat toast

--- src/index.js
+++ src/index.js
@@ -8,6 +8,18 @@ const getTargetElement = (element) => {
   return target
 }
 
+const toast = (text, time) => {
+  let toast = getTargetElement('#toast')
+  let toast_box = getTargetElement('.toast_box')
+  toast.innerHTML = text
+  toast_box.style.display = 'inline-block'
+  setTimeout(function () {
+    setTimeout(function () {
+      toast_box.style.display = 'none'
+    }, 1000)
+  }, time)
+}
+
 const getInputValue = () => {
   const input = getTargetElement('.copy-input')
   if (!input) return
@@ -21,19 +33,18 @@ const copy = (element) => {
   window.getSelection()?.addRange(range)
   document.execCommand('copy')
   window.getSelection()?.removeAllRanges()
-  alert('Copied!')
+  toast('Copied!', 1000)
 }
 
 const copyHandler = () => {
   const elementName = getInputValue()
   if (!elementName) {
-    alert('Please enter a value')
+    toast('Please enter a value', 1000)
     return
   }
   const target = getTargetElement(elementName)
-  console.log('!!!!', parent)
   if (!target) {
-    alert('Target element not found')
+    toast('Element not found', 1000)
     return
   }
   copy(target)
